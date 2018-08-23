0c81c35eec21c93a45f2623beae21acadc297cdd
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2018-08-23 18:28:39 +0300

    update

--- resources/views/app_header.blade.php
+++ resources/views/app_header.blade.php
@@ -37,7 +37,7 @@
                 <line x1="16" y1="8" x2="23" y2="1"></line>
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
             </svg>
-            <div class="d-none d-md-block">
+            <div class="d-none d-md-block" style="padding-left: 0.5rem">
                 <span class="header__phone">+7 495 455-33-99</span>
                 <a href="#" class="header__phone--callback">Обратный звонок</a>
             </div>
@@ -50,8 +50,9 @@
                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
                 <circle cx="12" cy="10" r="3"></circle>
             </svg>
-
-            <span class="header_adress d-none d-md-block">Москва, ул. Тверская, дом 256, офис 54</span>
+            <div class="d-none d-md-block" style="padding-left: 0.5rem">
+                <span class="header_adress d-none d-md-block">Москва, ул. Тверская, дом 256, офис 54</span>
+            </div>
         </div>
     </div>
 </header>
